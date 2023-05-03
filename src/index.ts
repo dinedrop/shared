1ea63692b4c12fb21e835fdd1ff4f199aec50ca5
Rhythm Shandlya <armaanbgp@gmail.com>
2023-05-03 22:04:16 +0530

    error handling, utils, toJSON

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import { logger, morgan } from './logger';
+import { ApiError, errorConverter, errorHandler } from './errors';
+import { catchAsync, pick, authLimiter } from './utils';
+import { toJSON } from './toJSON';
 
-export { logger, morgan };
+export {
+  logger,
+  morgan,
+  ApiError,
+  errorConverter,
+  errorHandler,
+  catchAsync,
+  pick,
+  authLimiter,
+  toJSON,
+};
